c2b53b41d7f25b81311e33c290d99c5628932be8
jasoncordis <cordisjason@gmail.com>
2021-04-11 14:20:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -153,7 +153,8 @@ function getContent($url, $geturl = false)
 <!DOCTYPE html>
 <html>
 <head>
-	<title>TikTok Video Downloader</title>
+	<title>Did Kanye Drop Yandhi Yet?</title>
+	<link rel="icon" href="kanyefav.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script></script>
