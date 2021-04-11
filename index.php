167cefca78ec76a358a13c81785da5b0711764e5
jasoncordis <cordisjason@gmail.com>
2021-04-11 14:13:18 -0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -32,23 +32,6 @@ body {
 }
 </style>
 
-<?php
-$handle = curl_init();
- 
-$url = "https://www.ladygaga.com";
- 
-// Set the url
-curl_setopt($handle, CURLOPT_URL, $url);
-// Set the result output to be a string.
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
- 
-$output = curl_exec($handle);
- 
-curl_close($handle);
-echo $output;
-echo "<h2>PHP is Fun!</h2>";
-?>
-
 </body>
 
 </html>
