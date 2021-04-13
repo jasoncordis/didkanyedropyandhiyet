545efede815e8cc6c433aae01c06fb8405e7a655
jasoncordis <cordisjason@gmail.com>
2021-04-12 16:57:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,40 @@ curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Basic '.base64_en
 
 $result=curl_exec($ch);
 echo $result;
+$arr = json_decode(result);
+$token = $arr->access_token;
+
+//setup the request, you can also use CURLOPT_URL
+$ch = curl_init('https://api.spotify.com/v1/search?q=Yandhi&type=album&market=US&limit=5&offset=0');
+
+// Returns the data/output as a string instead of raw data
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//Set your auth headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+   'Content-Type: application/json',
+   'Authorization: Bearer ' . $token
+   ));
+
+$json = curl_exec($ch);
+
+$json = json_decode($json, TRUE);
+foreach ($json['albums']['items'] as $item) {
+        if($item['artists'][0]['name'] == 'Kanye West'){
+            $bool = True;
+            break;
+        }
+        echo $item['artists'][0]['name'];
+        $bool = False;
+}
 ?>
+
+<script type="text/javascript">
+// boolean outputs "" if false, "1" if true
+var val = "<?php echo $bool ?>";
+var myString: string = String(bool);
+alert(myString);
+</script>
 
 <!DOCTYPE html>
 <html>
@@ -65,99 +98,5 @@ body {
 	<div class="text-center p-5">
 		<h1 class="mt-5">NO</h1>		
 	</div>
-	<?php
-		if (isset($_POST['tiktok-url']) && !empty($_POST['tiktok-url'])) {
-			$url = trim($_POST['tiktok-url']);
-			$resp = getContent($url);
-			//echo "$resp";
-			$check = explode('"downloadAddr":"', $resp);
-			if (count($check) > 1){
-				$contentURL = explode("\"",$check[1])[0];
-                $contentURL = str_replace("\\u0026", "&", $contentURL);
-				$thumb = explode("\"",explode('og:image" content="', $resp)[1])[0];
-				$username = explode('/',explode('"$pageUrl":"/@', $resp)[1])[0];
-				$create_time = explode(',', explode('"createTime":', $resp)[1])[0];
-				$dt = new DateTime("@$create_time");
-				$create_time = $dt->format("d M Y H:i:s A");
-				$videoKey = getKey($contentURL);
-				$cleanVideo = "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id=$videoKey&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4";
-				$cleanVideo = getContent($cleanVideo, true);
-				if (!file_exists("user_videos") && $store_locally){
-					mkdir("user_videos");
-				}
-				if ($store_locally){
-					?>
-                    <script type="text/javascript">
-                        $(document).ready(function(){
-                            $('#wmarked_link').text("Please wait ...");
-                            $.get('./<?php echo basename($_SERVER['PHP_SELF']); ?>?url=<?php echo urlencode($contentURL); ?>').done(function(data)
-                                {
-                                    $('#wmarked_link').removeAttr('disabled');
-                                    $('#wmarked_link').attr('onclick', 'window.location.href="' + data + '"');
-                                    $('#wmarked_link').text("Download Video");
-                                });
-                        });
-                    </script>
-                    <?php
-				}
-		?>
-		<script>
-		    $(document).ready(function(){
-		        $('html, body').animate({
-					    scrollTop: ($('#result').offset().top)
-					},1000);
-		    });
-		</script>
-	<div class="border m-3 mb-5" id="result">
-	    <div class="text-center"><br>Bot/Scraper Development Services: <a target="_blank" href="https://www.we-can-solve.com">We-Can-Solve.com</a></div>
-		<div class="row m-0 p-2">
-		<div class = video>	
-		<video controls="" autoplay="" name="media">
-    <source src= data type="video/mp4">
-			</video></div>
-			<div class="col-sm-5 col-md-5 col-lg-5 text-center"><img width="250px" height="250px" src="<?php echo $thumb; ?>"></div>
-			<div class="col-sm-6 col-md-6 col-lg-6 text-center mt-5"><ul style="list-style: none;padding: 0px">
-				<li>a video by <b>@<?php echo $username; ?></b></li>
-				<li>uploaded on <b><?php echo $create_time; ?></b></li>
-				<li><button id="wmarked_link" disabled="disabled" class="btn btn-primary mt-3" onclick="window.location.href='<?php if ($store_locally){ echo $filename;} else { echo $contentURL; } ?>'">Download Video</button> <button class="btn btn-info mt-3" onclick="window.location.href='<?php echo $cleanVideo; ?>'">Download Watermark Free!</button></li>
-				<li><div class="alert alert-primary mb-0 mt-3">If the video opens directly, try saving it by pressing CTRL+S or on phone, save from three dots in the bottom left corner</div></li>
-			</ul></div>
-		</div>
-	</div>
-	<?php
-			}
-			else
-			{
-				?>
-				<script>
-        		    $(document).ready(function(){
-        		        $('html, body').animate({
-        					    scrollTop: ($('#result').offset().top)
-        					},1000);
-        		    });
-        		</script>
-				<div class="mx-5 px-5 my-3" id="result">
-				    <div class="text-center"><br>Bot/Scraper Development Services: <a target="_blank" href="https://www.we-can-solve.com">We-Can-Solve.com</a></div>
-					<div class="alert alert-danger mb-0"><b>Please double check your url and try again.</b></div>
-				</div>
-
-				<?php
-			}
-		}
-	?>
-	<div class="m-5">
-		&nbsp;
-	</div>
-    <script type="text/javascript">
-        window.setInterval(function(){
-            if ($("input[name='tiktok-url']").attr("placeholder") == "https://www.tiktok.com/@username/video/1234567890123456789") {
-                $("input[name='tiktok-url']").attr("placeholder", "https://vm.tiktok.com/a1b2c3/");
-            }
-            else
-            {
-                $("input[name='tiktok-url']").attr("placeholder", "https://www.tiktok.com/@username/video/1234567890123456789");
-            }
-        }, 3000);
-    </script>
 </body>
 </html>
