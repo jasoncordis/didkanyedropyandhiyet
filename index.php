bd88b28bbaeccc83ff50abca9e698d49ff480ca4
jasoncordis <cordisjason@gmail.com>
2021-04-12 17:29:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
-<script type="text/javascript">
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Did Kanye Drop Yandhi Yet?</title>
+	<link rel="icon" href="kanyefav.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">
+</head>
+<style>
+body {
+  text-align: center;
+  background-image: url('kanye-west-yandhi-hologram-01.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+</style>
+<body>
+	<div id = "statusText">		
+	</div>
+	
+	<script type="text/javascript">
 function printStatus(status){
 	console.log(status);
 	document.getElementById("statusText").innerHTML = status;
 }
 </script>
+
+</body>
+</html>
 
 <?php
 
@@ -51,30 +79,3 @@ echo "<script>printStatus('YES')</script>";
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Did Kanye Drop Yandhi Yet?</title>
-	<link rel="icon" href="kanyefav.png">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">
-</head>
-<style>
-body {
-  text-align: center;
-  background-image: url('kanye-west-yandhi-hologram-01.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-}
-</style>
-<body>
-	<div id = "statusText">		
-	</div>
-</body>
-</html>
